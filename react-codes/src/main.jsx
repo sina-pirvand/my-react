762349077974adf20719fab6e9d9a973e4019b60
sina <sinaprvnd.webdev@gmail.com>
2023-08-07 22:08:44 +0330

    initial

---
+++ react-codes/src/main.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// import ReactDOM from "react-dom/client";
+
+// function App() {
+//   return <h1>Hello World</h1>;
+// }
+
+// const root = document.getElementById("root");
+// ReactDOM.createRoot(root).render(<App />);
